54724befd794d7e85b775f367edadbfee50c4309
motongxue <393888412@qq.com>
2023-11-09 11:24:03 +0800

    ✨ feat: 实现对多个IP地址进行ping操作

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"math"
 	"net"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -26,6 +27,12 @@ var (
 	totalTs      int64 = 0             // 总延迟
 )
 
+var analysisTemplate = `%s 的 Ping 统计信息:
+    数据包: 已发送 = %d，已接收 = %d，丢失 = %d (%.2f%% 丢失)，
+往返行程的估计时间(以毫秒为单位):
+    最短 = %dms，最长 = %dms，平均 = %dms
+`
+
 // 严格遵循ICMP报文结构顺序
 type ICMP struct {
 	Type     uint8  // 8位类型
@@ -37,70 +44,70 @@ type ICMP struct {
 
 func main() {
 	getCommandArgs()
-	destIp := os.Args[len(os.Args)-1]
-	conn, err := net.DialTimeout("ip:icmp", destIp, time.Duration(timeout)*time.Millisecond)
-	defer conn.Close()
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	fmt.Printf("正在 Ping %s [%s] 具有 %d 字节的数据:", destIp, conn.RemoteAddr(), size)
-	for i := 0; i < count; i++ {
-		sendCount++
-		icmpPacket := &ICMP{
-			Type:     typ,
-			Code:     code,
-			CheckSum: 0,
-			ID:       1,
-			Sequence: 1,
+	destIps := os.Args[len(os.Args)-1]
+	destIpArr := strings.Split(destIps, ",")
+	for _, destIp := range destIpArr {
+		conn, err := net.DialTimeout("ip:icmp", destIp, time.Duration(timeout)*time.Millisecond)
+		defer conn.Close()
+		if err != nil {
+			log.Fatal(err)
+			return
 		}
-		data := make([]byte, size)                          // 创建指定大小的字节切片
-		var buffer bytes.Buffer                             // 创建字节缓冲区
-		binary.Write(&buffer, binary.BigEndian, icmpPacket) // 将 ICMP 结构体的二进制表示写入缓冲区
-		buffer.Write(data)                                  // 将随机数据附加到缓冲区
-		data = buffer.Bytes()                               // 将缓冲区中的字节设置为 data 切片
-		checkSum := checkSum(data)                          // 计算校验和
-		data[2] = byte(checkSum >> 8)                       // 将校验和的高位字节写入 data 切片
-		data[3] = byte(checkSum)                            // 将校验和的低位字节写入 data 切片
+		fmt.Printf("正在 Ping %s [%s] 具有 %d 字节的数据:", destIp, conn.RemoteAddr(), size)
+		for i := 0; i < count; i++ {
+			sendCount++
+			icmpPacket := &ICMP{
+				Type:     typ,
+				Code:     code,
+				CheckSum: 0,
+				ID:       1,
+				Sequence: 1,
+			}
+			data := make([]byte, size)                          // 创建指定大小的字节切片
+			var buffer bytes.Buffer                             // 创建字节缓冲区
+			binary.Write(&buffer, binary.BigEndian, icmpPacket) // 将 ICMP 结构体的二进制表示写入缓冲区
+			buffer.Write(data)                                  // 将随机数据附加到缓冲区
+			data = buffer.Bytes()                               // 将缓冲区中的字节设置为 data 切片
+			checkSum := checkSum(data)                          // 计算校验和
+			data[2] = byte(checkSum >> 8)                       // 将校验和的高位字节写入 data 切片
+			data[3] = byte(checkSum)                            // 将校验和的低位字节写入 data 切片
 
-		conn.SetDeadline(time.Now().Add(time.Duration(timeout) * time.Millisecond))
-		t1 := time.Now()
-		n, err := conn.Write(data)
-		if err != nil {
-			failCount++
-			log.Fatal(err)
-			continue
+			conn.SetDeadline(time.Now().Add(time.Duration(timeout) * time.Millisecond))
+			t1 := time.Now()
+			n, err := conn.Write(data)
+			if err != nil {
+				failCount++
+				log.Fatal(err)
+				continue
+			}
+			buf := make([]byte, 65535)
+			n, err = conn.Read(buf)
+			if err != nil {
+				failCount++
+				log.Fatal(err)
+				continue
+			}
+			successCount++
+			ts := time.Since(t1).Milliseconds()
+			if minTs > ts || minTs == math.MaxInt32 {
+				minTs = ts
+			}
+			if maxTs < ts {
+				maxTs = ts
+			}
+			totalTs += ts
+			fmt.Printf("来自 %s 的回复: 字节=%d 时间=%dms TTL=%d\n", conn.RemoteAddr(), n-28, ts, buf[8])
+			time.Sleep(time.Second)
 		}
-		buf := make([]byte, 65535)
-		n, err = conn.Read(buf)
-		if err != nil {
-			failCount++
-			log.Fatal(err)
-			continue
-		}
-		successCount++
-		ts := time.Since(t1).Milliseconds()
-		if minTs > ts || minTs == math.MaxInt32 {
-			minTs = ts
-		}
-		if maxTs < ts {
-			maxTs = ts
-		}
-		totalTs += ts
-		fmt.Printf("来自 %s 的回复: 字节=%d 时间=%dms TTL=%d\n", conn.RemoteAddr(), n-28, ts, buf[8])
-		time.Sleep(time.Second)
+		fmt.Printf(analysisTemplate,
+			conn.RemoteAddr(),
+			sendCount, successCount,
+			failCount,
+			float64(failCount)/float64(sendCount),
+			minTs,
+			maxTs,
+			totalTs/int64(sendCount))
 	}
-	fmt.Printf(`%s 的 Ping 统计信息:
-    数据包: 已发送 = %d，已接收 = %d，丢失 = %d (%.2f%% 丢失)，
-往返行程的估计时间(以毫秒为单位):
-    最短 = %dms，最长 = %dms，平均 = %dms`,
-		conn.RemoteAddr(),
-		sendCount, successCount,
-		failCount,
-		float64(failCount)/float64(sendCount),
-		minTs,
-		maxTs,
-		totalTs/int64(sendCount))
 }
 
 func checkSum(data []byte) uint16 {
